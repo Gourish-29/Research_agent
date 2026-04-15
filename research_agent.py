c0add1dec458bac0381c5baa4354d1ffce78333a
Gourish-29 <gourishanagire@gmail.com>
2026-04-15 18:26:05 +0530

    first commit

---
+++ research_agent.py
@@ -0,0 +1,59 @@
+# Import the required libraries
+import streamlit as st
+from agno.agent import Agent
+from agno.run.agent import RunOutput
+from agno.models.openai import OpenAIChat
+from agno.team import Team
+from agno.tools.duckduckgo import DuckDuckGoTools
+from agno.tools.hackernews import HackerNewsTools
+import os
+
+# Set up the Streamlit app
+st.title("Multi-Agent AI Researcher 🔍🤖")
+st.caption("This app allows you to research top stories and users on HackerNews and write blogs, reports and social posts.")
+
+# Get OpenAI API key from user
+openai_api_key = st.text_input("OpenAI API Key", type="password")
+os.environ["OPENAI_API_KEY"] = openai_api_key
+
+if openai_api_key:
+    hn_researcher = Agent(
+        name="HackerNews Researcher",
+        model=OpenAIChat(id="gpt-4o-mini"),
+        role="Gets top stories from hackernews.",
+        tools=[HackerNewsTools()],
+    )
+
+    web_searcher = Agent(
+        name="Web Searcher",
+        model=OpenAIChat(id="gpt-4o-mini"),
+        role="Searches the web for information on a topic",
+        tools=[DuckDuckGoTools()],
+        add_datetime_to_context=True,
+    )
+
+  
+
+    hackernews_team = Team(
+        name="HackerNews Team",
+        model=OpenAIChat(id="gpt-4o-mini"),
+        members=[hn_researcher, web_searcher],
+        instructions=[
+            "First, search hackernews for what the user is asking about.",
+           
+            "Important: you must provide the article reader with the links to read.",
+            "Then, ask the web searcher to search for each story to get more information.",
+            "Finally, provide a thoughtful and engaging summary.",
+        ],
+        markdown=True,
+        debug_mode=True,
+        show_members_responses=True,
+    )
+
+    # Input field for the report query
+    query = st.text_input("Enter your report query")
+
+    if query:
+        # Get the response from the assistant
+        response: RunOutput = hackernews_team.run(query, stream=False)
+        st.write(response.content)
